5115f56220c891e0c09de5ba821431884291ec3b
Henri <henri97@outlook.com>
2018-01-18 23:02:38 +0100

    Date-range of entries now in the PDF! Fixed bug with folder.

--- create_journal_latex.py
+++ create_journal_latex.py
@@ -15,7 +15,16 @@ output_dir = 'latex_output/'
 author = "Henri Chilla"
 locale.setlocale(locale.LC_ALL, "de_DE")
 
+def get_date_of_filename(filename):
+    filename = file.split('.')[0]
+    if '+' in filename:
+        date = datetime.strptime(filename.split('+')[0], "%Y-%m-%d %H:%M:%S")
+    else:
+        date = datetime.strptime(filename, "%Y-%m-%d %H:%M:%S")
+    return date
+
 def process_file(file):
+    filename = file.split('.')[0]
     # Convert from markdown to LaTeX with pandoc
     os.system("pandoc --wrap=none -o \"" + dir + '/' + output_dir + filename + '.tex\"  \"' + dir + '/markdown_output/' + filename + '.md\"' )
     current_entry = open(dir + '/' + output_dir + filename + '.tex' , 'r')
@@ -44,8 +53,8 @@ if args.year:
     print "Year " + str(args.year) + " will be processed.\n"
 
 # Create output folder if it not exists
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+if not os.path.exists(dir + '/' + output_dir):
+    os.makedirs(dir + '/' + output_dir)
 if not os.path.exists(output_file_folder):
     os.makedirs(output_file_folder)
 
@@ -58,30 +67,35 @@ except IOError:
     sys.exit()
 print('read journal template')
 
-template_tex = template.read()
-# Add author
-template_tex = re.sub(r"\\author{\w.*}", r"\\author{" + author + "}", template_tex)
-
 files_entries = listdir(dir + '/markdown_output')
-document.write(template_tex + '\n')
-
 # sort list by datetime
 files_entries.sort()
 
 for file in files_entries:
-    if file.endswith('.md'):
-        filename = file.split('.')[0]
-        if not args.year:
+    if not file.endswith('.md'):
+        files_entries.remove(file)
+
+template_tex = template.read()
+# Add author
+template_tex = re.sub(r"\\author{\w*}", r"\\author{" + author + "}", template_tex)
+# Add correct date
+date_format = "%Y-%m-%d %H:%M:%S"
+if args.year:
+    template_tex = re.sub(r"\\date{\w*}", r"\\date{" + str(args.year) + "}", template_tex)
+else:
+    template_tex = re.sub(r"\\date{\w*}", r"\\date{" + get_date_of_filename(files_entries[0]).strftime("%d.%m.%Y") + " - " + get_date_of_filename(files_entries[-1]).strftime("%d.%m.%Y") + "}", template_tex)
+
+document.write(template_tex + '\n')
+
+for file in files_entries:
+    if not args.year:
+        process_file(file)
+    else:
+        creation_date = get_date_of_filename(file)
+        if args.year == creation_date.year:
             process_file(file)
         else:
-            if '+' in filename:
-                creation_date = datetime.strptime(filename.split('+')[0], "%Y-%m-%d %H:%M:%S")
-            else:
-                creation_date = datetime.strptime(filename, "%Y-%m-%d %H:%M:%S")
-            if args.year == creation_date.year:
-                process_file(file)
-            else:
-                continue
+            continue
 
 document.write('\n\\end{document}')
 print('writed journal_output.tex\nFinished.')
